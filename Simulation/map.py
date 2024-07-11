d8b073d2b7cdfcef6fe23a492e9011496ea3299e
m_salmani <m.salmani78@ut.ac.ir>
2024-07-11 16:09:25 +0330

    add map

--- Simulation/map.py
+++ Simulation/map.py
@@ -1,132 +1,174 @@
 import xml.etree.ElementTree as ET
+
+# import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import math
 import shapely
 from shapely.geometry import LineString, Point, Polygon
 
 
-import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
-import numpy as np
-from shapely.geometry import LineString, Point, Polygon
+def init_map(address):
 
-class Map:
-    def __init__(self, map_file):
-        self.objects = self.parse_map(map_file)
-        self.add_offset()
-        self.xmin, self.ymin, self.xmax, self.ymax = self.get_map_coordinates()
-        print("$$$ MAP", self.xmin, self.ymin, self.xmax, self.ymax)
+    tree = ET.parse(address)
+    root = tree.getroot()
 
-    def parse_map(self, map_file):
-        tree = ET.parse(map_file)
-        root = tree.getroot()
-        objects = {}
-        for model in root.findall("./world/model"):
-            for child in model:
-                if child.tag == "pose":
-                    offset = child.text.split()
-                    self.x_offset, self.y_offset = float(offset[0]), float(offset[1])
+    rects = []
+    centers = []
 
-                if child.tag == "link":
-                    pose = child.find("pose")  # Center
-                    size = child.find("collision/geometry/box/size")  # Size
-                    if pose is not None and size is not None:
-                        name = child.attrib["name"]
-                        xc, yc, z, roll, pitch, yaw = map(float, pose.text.split())  # Center of each cube (Pose)
-                        w, h, _ = map(float, size.text.split())  # Width & Height (& z - length) of each cube
-                        objects[name] = self.get_points(xc, yc, yaw, w, h)
-        return objects
+    for links in root[0].iter("model"):
 
-    def get_points(self, xc, yc, theta, w, h):
-        upper_right = [
-            xc + w * np.sin(theta) / 2 + h * np.cos(theta) / 2,
-            yc + h * np.sin(theta) / 2 - w * np.cos(theta) / 2,
-        ]
-        upper_left = [
-            xc - w * np.sin(theta) / 2 + h * np.cos(theta) / 2,
-            yc + h * np.sin(theta) / 2 + w * np.cos(theta) / 2,
-        ]
-        lower_right = [
-            xc + w * np.sin(theta) / 2 - h * np.cos(theta) / 2,
-            yc - h * np.sin(theta) / 2 - w * np.cos(theta) / 2,
-        ]
-        lower_left = [
-            xc - w * np.sin(theta) / 2 - h * np.cos(theta) / 2,
-            yc - h * np.sin(theta) / 2 + w * np.cos(theta) / 2,
-        ]
-        return {
-            "upper_right": upper_right,
-            "upper_left": upper_left,
-            "lower_right": lower_right,
-            "lower_left": lower_left,
-        }
+        try:
 
-    def add_offset(self):
-        for obj in self.objects.values():
-            for point in obj.values():
-                point[0] += self.x_offset
-                point[1] += self.y_offset
+            for link in links:
+                if link.tag == "pose":
+                    _global_map_pose = link.text.split(" ")
+                    if _global_map_pose[0] != "0":
+                        global_map_pose = _global_map_pose
+                        print(global_map_pose)
 
-    def get_lines(self):
-        lines = []
-        for obj in self.objects.values():
-            lines.append([obj["upper_right"], obj["lower_right"]])
-            lines.append([obj["lower_right"], obj["lower_left"]])
-            lines.append([obj["lower_left"], obj["upper_left"]])
-            lines.append([obj["upper_left"], obj["upper_right"]])
-        return lines
+                # for link in links:
+                if link.tag == "link":
 
-    def draw_map(self):
-        for obj in self.objects.values():
-            x_coords = [obj["upper_right"][0], obj["lower_right"][0], obj["lower_left"][0], obj["upper_left"][0]]
-            y_coords = [obj["upper_right"][1], obj["lower_right"][1], obj["lower_left"][1], obj["upper_left"][1]]
+                    geometry = None
+                    pose = None
+                    for _pose in link.iter("pose"):
+                        pose = _pose.text.split(" ")
+                        break
+                    for collision in link.iter("collision"):
+                        geometry = collision[3][0][0].text.split(" ")
 
-            # Draw the edges of the object
-            plt.plot(x_coords + [x_coords[0]], y_coords + [y_coords[0]], c="black")
+                    p1 = [
+                        float(pose[0])
+                        + (float(geometry[0]) * math.cos(float(pose[5])) / 2)
+                        + float(geometry[1]) * math.sin(float(pose[5])) / 2,
+                        float(pose[1])
+                        + float(geometry[0]) * math.sin(float(pose[5])) / 2
+                        - float(geometry[1]) * math.cos(float(pose[5])) / 2,
+                    ]
 
-            # Fill the object
-            plt.fill(x_coords, y_coords, facecolor="gray", edgecolor="black", linewidth=1)
+                    p2 = [
+                        float(pose[0])
+                        + float(geometry[0]) * math.cos(float(pose[5])) / 2
+                        - float(geometry[1]) * math.sin(float(pose[5])) / 2,
+                        float(pose[1])
+                        + float(geometry[0]) * math.sin(float(pose[5])) / 2
+                        + float(geometry[1]) * math.cos(float(pose[5])) / 2,
+                    ]
 
-    def point_in_object(self, x, y):
-        point = Point(x, y)
-        for obj in self.objects.values():
-            points = [obj["upper_right"], obj["lower_right"], obj["lower_left"], obj["upper_left"]]
-            poly = Polygon(points)
-            if point.within(poly) or poly.touches(point):
-                return True
+                    p3 = [
+                        float(pose[0])
+                        - float(geometry[0]) * math.cos(float(pose[5])) / 2
+                        + float(geometry[1]) * math.sin(float(pose[5])) / 2,
+                        float(pose[1])
+                        - float(geometry[0]) * math.sin(float(pose[5])) / 2
+                        - float(geometry[1]) * math.cos(float(pose[5])) / 2,
+                    ]
+
+                    p4 = [
+                        float(pose[0])
+                        - float(geometry[0]) * math.cos(float(pose[5])) / 2
+                        - float(geometry[1]) * math.sin(float(pose[5])) / 2,
+                        float(pose[1])
+                        - float(geometry[0]) * math.sin(float(pose[5])) / 2
+                        + float(geometry[1]) * math.cos(float(pose[5])) / 2,
+                    ]
+
+                    rects.append([p1, p2, p3, p4])
+                    centers.append([pose[0], pose[1]])
+
+                    # plt.plot([p1[0],p2[0],p4[0],p3[0],p1[0]],[p1[1],p2[1],p4[1],p3[1],p1[1]])
+
+        except Exception as e:
+            pass
+
+    # print(global_map_pose)
+
+    return rects, global_map_pose, map_boundry(centers)
+
+
+def find_intersection(p1, p2, p3, p4):
+
+    line1 = LineString([tuple(p1), tuple(p2)])
+    line2 = LineString([tuple(p3), tuple(p4)])
+
+    int_pt = line1.intersection(line2)
+    if int_pt:
+        point_of_intersection = int_pt.x, int_pt.y
+        return point_of_intersection
+    else:
         return False
 
-    def get_map_coordinates(self):
-        x = []
-        y = []
-        for obj in self.objects.values():
-            for point in obj.values():
-                x.append(point[0])
-                y.append(point[1])
-        return min(x), min(y), max(x), max(y)
 
-    def point_in_map(self, x, y):
-        return self.xmin <= x <= self.xmax and self.ymin <= y <= self.ymax
+def convert_point_to_line(rects):
+    lines = []
+    for points in rects:
+        lines.append([points[0], points[1]])
+        lines.append([points[1], points[3]])
+        lines.append([points[3], points[2]])
+        lines.append([points[2], points[0]])
+    return lines
 
-    def valid_point(self, x, y):
-        return not self.point_in_object(x, y) and self.point_in_map(x, y)
 
-    def find_intersection(self, point_1, point_2, point_3, point_4):
-        line1 = LineString([tuple(point_1), tuple(point_2)])
-        line2 = LineString([tuple(point_3), tuple(point_4)])
-        intersection = line1.intersection(line2)
-        return intersection
+def add_offset(rects, offset):
+    new_rects = []
+    for points in rects:
+        new_rects.append(
+            [
+                [points[0][0] + offset[0], points[0][1] + offset[1]],
+                [points[1][0] + offset[0], points[1][1] + offset[1]],
+                [points[2][0] + offset[0], points[2][1] + offset[1]],
+                [points[3][0] + offset[0], points[3][1] + offset[1]],
+            ]
+        )
+    return new_rects
 
-    def find_closest_intersection(self, x1, y1, x2, y2, max_distance):
-        min_distance = max_distance
-        for line in self.get_lines():
-            intersection = self.find_intersection((x1, y1), (x2, y2), line[0], line[1])
-            if intersection:
-                distance = np.sqrt((intersection.x - x1) ** 2 + (intersection.y - y1) ** 2)
-                if distance < min_distance:
-                    min_distance = distance
-        return min_distance
 
-# Example usage:
-# map_file = 'path_to_your_map_file.xml'
-# my_map = Map(map_file)
+def convert_to_poly(rects):
+    polygons = []
+    for points in rects:
+        polygons.append(
+            Polygon(
+                [tuple(points[0]), tuple(points[1]), tuple(points[3]), tuple(points[2])]
+            )
+        )
+    return polygons
+
+
+def check_is_collition(point, rects):
+    p = Point(tuple(point))
+    for rect in rects:
+        if rect.contains(p):
+            return True
+    return False
+
+
+def map_boundry(centers):
+    X = []
+    Y = []
+
+    for item in centers:
+        X.append(float(item[0]))
+        Y.append(float(item[1]))
+
+    return min(X), max(X), min(Y), max(Y)
+
+
+def out_of_range(particle, offset, map_boundry):
+    if (
+        particle[0] - offset[0] > map_boundry[1]
+        or particle[0] - offset[0] < map_boundry[0]
+    ):
+        return True
+    elif (
+        particle[1] - offset[1] > map_boundry[3]
+        or particle[1] - offset[1] < map_boundry[2]
+    ):
+        return True
+    else:
+        return False
+
+
+def plot_map(rects):
+    for rect in rects:
+        rect = list(zip(*rect))
+        plt.plot(rect[1], rect[0], c="black")
